9d3887bd7a77b3f35d53831b65fac0a1c0a008e3
THIAGO PEREIRA DOS SANTOS <tthiagopereira7@gmail.com>
2024-03-16 15:49:44 -0400

    ci: Change event to pull_request to develop

--- math.go
+++ math.go
@@ -7,10 +7,10 @@ func main() {
 	fmt.Println(Sub(10, 10))
 }
 
-func Soma(a, b int) int {
-	return a + b
-}
-
 func Sub(a, b int) int {
 	return a - b
+}
+
+func Soma(a, b int) int {
+	return a + b
 }
